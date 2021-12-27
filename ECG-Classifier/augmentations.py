9e6f77be97b77cdef2209687dd5a4ebe843fa51e
Rabindra Nepal <nepalrabindra89@gmail.com>
2021-12-26 20:32:29 -0500

    initial upload

---
+++ ECG-Classifier/augmentations.py
@@ -0,0 +1,51 @@
+import numpy as np
+from scipy.signal import butter, sosfilt
+
+def scaling(signal, sigma=0.05):
+    '''Introduces some noise into the signal'''
+    scale_factor = np.random.normal(loc=1.0, scale=sigma, size=(1, signal.shape[1]))
+    noise = np.matmul(np.ones((signal.shape[0], 1)), scale_factor)
+    return signal * noise
+
+def vertical_flip(signal):
+    '''
+    Input: signal
+    Return: vertically flipped signal
+    '''
+    return signal[::-1, :]
+
+def shift(signal, interval=20):
+    '''
+    Input: signal
+    Return: shited signal by interval
+    '''
+    for col in range(signal.shape[1]):
+        offset = np.random.choice(range(-interval, interval))/100
+        signal[:, col] += offset
+    return signal
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    sos = butter(order, [low, high], analog = False, btype="band", output="sos")
+    return sos
+
+def butter_bandpass_filter(signal, lowcut, highcut, fs, order=5):
+    sos = butter_bandpass(lowcut, highcut, fs, order=order)
+    transformed_signal = np.zeros([signal.shape[0], signal.shape[1]])
+    for i in range(signal.shape[1]): 
+        transformed_signal[:, i] = sosfilt(sos, signal[:, i])  
+    return transformed_signal
+
+def transform(signal, train=True):
+    '''
+    Input: a signal to transform
+    Output: for a training signla: it is randomly transformed and returned 
+    '''
+    if train:
+        if np.random.randn() > 0.5: signal = scaling(signal)
+        if np.random.randn() > 0.3: signal = vertical_flip(signal)
+        if np.random.randn() > 0.5: signal = shift(signal)
+        if np.random.randn() > 0.3: signal = butter_bandpass_filter(signal, 0.05, 46, 256)
+    return signal
